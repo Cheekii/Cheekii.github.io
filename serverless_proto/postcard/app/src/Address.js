ea74f5891f22ecc4a897312993c9c1f6aa993886
Braden <braden@mund.ca>
2018-01-07 03:08:34 -0700

    temp address change

--- serverless_proto/postcard/app/src/Address.js
+++ serverless_proto/postcard/app/src/Address.js
@@ -1,37 +1,20 @@
 import React, { Component } from 'react';
-import { FormField } from 'react-form';
+import { Form, Text, Select, NestedForm } from 'react-form';
+import {CountryDropdown, RegionDropdown} from './rcrs/rcrs';
 import './Address.css';
 
-class AddressWrapper extends Component {
+class Address extends Component {
   constructor( props ) {
     super( props );
 
-    const {
-      fieldApi,
-      onInput
-    } = this.props;
-
-    const {
-      getValue,
-      getError,
-      getWarning,
-      getSuccess,
-      setValue,
-      setTouched,
-    } = fieldApi;
-
-    const error = getError();
-    const warning = getWarning();
-    const success = getSuccess();
-
     this.state = {
-      fullname: "",
-      address_1: "",
-      address_2: "",
-      city: "",
-      country: "",
-      region: "",
-      zip: ""
+      // fullname: "",
+      // address_1: "",
+      // address_2: "",
+      // city: "",
+      // country: "",
+      // region: "",
+      // zip: ""
     };
     this.handleChange = this.handleChange.bind(this);
   }
@@ -45,105 +28,164 @@ class AddressWrapper extends Component {
   }
   render() {
     return (
-      <div className="Address">
-        <div className="form-group">
-          <label htmlFor="full-name">Full Name:</label>
-          <input
-            type="text"
-            name="fullname"
-            value={this.state.fullname}
-            onChange={this.handleChange}
-            id="full-name"
-            placeholder="full name"
-            className="form-control"
-            required />
-        </div>
+      <NestedForm field={this.props.field}>
+        <Form>
+          { formApi => (
+            <div className="Address">
+              <div className="form-group">
+                <label htmlFor="full-name">Full Name:</label>
+                <Text
+                  field="fullname"
+                  id="full-name"
+                  placeholder="full name"
+                  className="form-control"
+                  required/>
+                {/* <input
+                  type="text"
+                  name="fullname"
+                  value={this.state.fullname}
+                  onChange={this.handleChange}
+                  id="full-name"
+                  placeholder="full name"
+                  className="form-control"
+                  required /> */}
+              </div>
 
-        <div className="form-group">
-          <label htmlFor="address-line-1">Address Line 1:</label>
-          <input
-            type="text"
-            name="address_1"
-            value={this.state.address_1}
-            onChange={this.handleChange}
-            id="address-line-1"
-            placeholder="address line 1"
-            className="form-control"
-            required />
-          <small className="form-text text-muted">
-            Street address, P.O. box, company name, c/o
-          </small>
-        </div>
+              <div className="form-group">
+                <label htmlFor="address-line-1">Address Line 1:</label>
+                <Text
+                  field="address_1"
+                  id="address-line-1"
+                  placeholder="address line 1"
+                  className="form-control"
+                  required/>
+                {/* <input
+                  type="text"
+                  name="address_1"
+                  value={this.state.address_1}
+                  onChange={this.handleChange}
+                  id="address-line-1"
+                  placeholder="address line 1"
+                  className="form-control"
+                  required /> */}
+                <small className="form-text text-muted">
+                  Street address, P.O. box, company name, c/o
+                </small>
+              </div>
 
-        <div className="form-group">
-          <label htmlFor="address-line-2" >Address Line 2:</label>
-          <input
-            type="text"
-            name="address_2"
-            value={this.state.address_2}
-            onChange={this.handleChange}
-            id="address-line-2"
-            placeholder="address line 2"
-            className="form-control" />
-          <small className="form-text text-muted">
-            Apartment, suite , unit, building, floor, etc.
-          </small>
-        </div>
+              <div className="form-group">
+                <label htmlFor="address-line-2" >Address Line 2:</label>
+                <Text
+                  field="address_2"
+                  id="address-line-2"
+                  placeholder="address line 2"
+                  className="form-control" />
+                {/* <input
+                  type="text"
+                  name="address_2"
+                  value={this.state.address_2}
+                  onChange={this.handleChange}
+                  id="address-line-2"
+                  placeholder="address line 2"
+                  className="form-control" /> */}
+                <small className="form-text text-muted">
+                  Apartment, suite , unit, building, floor, etc.
+                </small>
+              </div>
 
-        <div className="form-group">
-          <label htmlFor="city">City</label>
-          <input
-            type="text"
-            name="city"
-            value={this.state.city}
-            onChange={this.handleChange}
-            id="city"
-            placeholder="city"
-            className="form-control"
-            required />
-        </div>
+              <div className="form-group">
+                <label htmlFor="city">City</label>
+                <Text
+                  field="city"
+                  id="city"
+                  placeholder="city"
+                  className="form-control"
+                  required/>
+                {/* <input
+                  type="text"
+                  name="city"
+                  value={this.state.city}
+                  onChange={this.handleChange}
+                  id="city"
+                  placeholder="city"
+                  className="form-control"
+                  required /> */}
+              </div>
 
-        <div className="form-row">
-          <div className="form-group col-md-5">
-            <label htmlFor="country">Country</label>
-            <select
-              type="text"
-              name="country"
-              value={this.state.country}
-              onChange={this.handleChange}
-              id="country"
-              className="form-control gds-cr"
-              country-data-region-id="region"
-              required />
-          </div>
-          <div className="form-group col-md-4">
-            <label htmlFor="region">State / Province / Region</label>
-            <select
-              type="text"
-              name="state"
-              value={this.state.state}
-              onChange={this.handleChange}
-              id="region"
-              className="form-control"
-              required />
-          </div>
-          <div className="form-group col-md-3">
-            <label htmlFor="zip">Zip / Postal Code</label>
-            <input
-              type="text"
-              name="zip"
-              value={this.state.zip}
-              onChange={this.handleChange}
-              id="zip"
-              className="form-control"
-              required />
-          </div>
-        </div>
-      </div>
+              <div className="form-row">
+                <div className="form-group col-md-5">
+                  <label htmlFor="country">Country</label>
+                  {/* <Select
+                    field="country"
+                    id="country"
+                    className="form-control gds-cr"
+                    country-data-region-id="region"
+                    options={[]}
+                    required /> */}
+                  <CountryDropdown
+                    field="country"
+                    id="country"
+                    whitelist={["CA"]}
+                    className="form-control"
+                    required/>
+                  {/* <select
+                    type="text"
+                    name="country"
+                    value={this.state.country}
+                    onChange={this.handleChange}
+                    id="country"
+                    className="form-control gds-cr"
+                    country-data-region-id="region"
+                    required /> */}
+                </div>
+                <div className="form-group col-md-4">
+                  <label htmlFor="region">State / Province / Region</label>
+                  {/* <Select
+                    field="state"
+                    id="region"
+                    className="form-control"
+                    country-data-region-id="region"
+                    options={[]}
+                    required /> */}
+                  <RegionDropdown
+                    field="region"
+                    id="region"
+                    country={formApi.values.country}
+                    value={formApi.values.region}
+                    className="form-control"
+                    required/>
+                  {/* <select
+                    type="text"
+                    name="state"
+                    value={this.state.state}
+                    onChange={this.handleChange}
+                    id="region"
+                    className="form-control"
+                    required /> */}
+                </div>
+                <div className="form-group col-md-3">
+                  <label htmlFor="zip">Zip / Postal Code</label>
+                  {/* <input
+                    type="text"
+                    name="zip"
+                    value={this.state.zip}
+                    onChange={this.handleChange}
+                    id="zip"
+                    className="form-control"
+                    required /> */}
+                  <Text
+                    field="zip"
+                    id="zip"
+                    className="form-control"
+                    required/>
+                </div>
+              </div>
+            </div>
+        )}
+      </Form>
+    </NestedForm>
     );
   }
 }
-
-const Address = FormField(AddressWrapper);
 
 export default Address;
--- serverless_proto/postcard/app/src/App.js
+++ serverless_proto/postcard/app/src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import Address from './Address';
-import {Form} from 'react-form';
-
-// import { Form, Text } from 'react-form';
+import {Form, TextArea} from 'react-form';
+import {CountryDropdown} from './rcrs/rcrs';
+import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -14,19 +14,78 @@ class App extends Component {
 
   handleSubmit(event) {
     const target = event.target;
-    console.log(target);
     // this.setState( { submittedValues });
   }
 
   render() {
     return (
       <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
         <div className="container">
           <Form onSubmit={this.handleSubmit}>
             { formApi => (
               <form onSubmit={formApi.submitForm} className="was-validated">
-                <Address />
+                <div id="accordion" role="tablist">
+
+                  <div className="card">
+                    <div class="card-header" role="tab" id="headingOne">
+                      <h5 class="mb-0">
+                        <a data-toggle="collapse" href="#collapseOne" role="button" aria-expanded="true" aria-controls="collapseOne">
+                          From Mailing Address
+                        </a>
+                      </h5>
+                    </div>
+
+                    <div id="collapseOne" class="collapse show" role="tabpanel" aria-labelledby="headingOne" data-parent="#accordion">
+                      <div class="card-body">
+                        <Address field="fromAddress"/>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className="card">
+                    <div class="card-header" role="tab" id="headingTwo">
+                      <h5 class="mb-0">
+                        <a data-toggle="collapse" href="#collapseTwo" role="button" aria-expanded="true" aria-controls="collapseTwo">
+                          To Mailing Address
+                        </a>
+                      </h5>
+                    </div>
+
+                    <div id="collapseTwo" class="collapse show" role="tabpanel" aria-labelledby="headingTwo" data-parent="#accordion">
+                      <div class="card-body">
+                        <Address field="toAddress"/>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className="card">
+                    <div class="card-header" role="tab" id="headingThree">
+                      <h5 class="mb-0">
+                        <a data-toggle="collapse" href="#collapseThree" role="button" aria-expanded="true" aria-controls="collapseThree">
+                          Message
+                        </a>
+                      </h5>
+                    </div>
+
+                    <div id="collapseThree" class="collapse show" role="tabpanel" aria-labelledby="headingThree" data-parent="#accordion">
+                      <div class="card-body">
+                        <TextArea
+                          field="message"
+                          name="message"
+                          className="form-control"
+                          rows="10"
+                          required/>
+                      </div>
+                    </div>
+                  </div>
+
+                </div>
+
                 <button type="submit" className="btn btn-primary">Submit</button>
+
               </form>
             )}
           </Form>
